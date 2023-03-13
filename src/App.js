11faee42cb7d54c8e67bad8a4f1e4ad0f37070e5
Buyaki <rittahbuyaki@gmail.com>
2023-03-13 11:27:31 +0300

    Add react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
+import { Route, Switch } from 'react-router-dom';
+import { useState, useEffect } from 'react';
+import Header from './Header';
+import Nav from './Nav';
+import Footer from './Footer';
+import Home from './Home';
+import NewPost from './NewPost';
+import PostPage from './PostPage';
+import About from './About';
+import Missing from './Missing';
+
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        
-      </header>
     </div>
   );
 }
