c8584be13a9ab4ba05e583d209a9aa20dfd92915
Buyaki <rittahbuyaki@gmail.com>
2023-03-14 13:35:17 +0300

    Add functionality to the Post component

--- src/Post.js
+++ src/Post.js
@@ -1,6 +1,20 @@
-const Post = () => {
+import { Link } from "react-router-dom"
+
+const Post = ({post}) => {
   return (
-    <div>Post</div>
+    <article className="post">
+      <Link to={`/post/${post.id}`}>
+        <h2 className="mb-3">{post.title}</h2> 
+        <p className="postDate mb-3">{post.datetime}</p>
+      </Link>
+      <p className="postBody mb-3">{
+        (post.body).length <= 25 
+          ? post.body
+          : `${(post.body).slice(0, 25)}....`
+        }
+      </p>
+      <hr />
+    </article>
   )
 }
 
