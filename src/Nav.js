aa6a7b46f54213488008090cb115bf07a4e64f5d
Buyaki <rittahbuyaki@gmail.com>
2023-03-14 12:06:34 +0300

    Add a form in the navigation component

--- src/Nav.js
+++ src/Nav.js
@@ -1,7 +1,17 @@
-const Nav = () => {
+const Nav = ({search, setSearch}) => {
     return (
-      <nav>
-        <h1>Nav</h1>
+      <nav className="Nav">
+        <form className="searchForm" onSubmit={(e) => e.preventDefault()}>
+          <label htmlFor="search" className="sr-only">Search Posts</label>
+          <input
+            className="searchInputField"
+            id="search" 
+            type="text" 
+            placeholder="Search Posts"
+            value={search}
+            onChange={(e) => setSearch(e.target.value)}
+          />
+        </form>
       </nav>
     )
   }
