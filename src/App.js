835c74ba4185bc3d9831f1844c0b505d0328bff5
Buyaki <rittahbuyaki@gmail.com>
2023-03-15 13:53:19 +0300

    Add form in the NewPost component to allow user to enter the title and body of the post

--- src/App.js
+++ src/App.js
@@ -7,7 +7,8 @@ import About from "./About";
 import Missing from "./Missing";
 import Footer from "./Footer";
 import { Routes, Route, useNavigate } from 'react-router-dom';
-import { useState } from "react";
+import { useState } from "react"
+import { format } from "date-fns";
 
 function App() {
   const [posts, setPosts] = useState([
@@ -38,12 +39,26 @@ function App() {
   ])
   const [search, setSearch] = useState('')
   const [searchResults, setSearchResults] = useState([])
+  const [postTitle, setPostTitle] = useState('')
+  const [postBody, setPostBody] = useState('')
   const navigate = useNavigate();
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    const id = posts.length ? posts[posts.length - 1].id + 1 : 1
+    const datetime = format(new Date(), 'MMMM dd, yyyy pp')
+    const newPost = {id, title: postTitle, datetime, body:  postBody}
+    const allPosts = [...posts, newPost]
+    setPosts(allPosts);
+    setPostTitle('')
+    setPostBody('')
+    navigate('/')
+  }
 
   const handleDelete = (id) => {
     const postsList = posts.filter(post => post.id !== id)
     setPosts(postsList)
-    navigate('/');
+    navigate('/')
   }
 
   return (
@@ -54,7 +69,13 @@ function App() {
       <Routes>
         <Route path="/" element={<Home posts={posts} />} />
     
-        <Route path="/post" element={<NewPost />} />
+        <Route path="/post" element={<NewPost 
+          postTitle={postTitle}
+          setPostTitle={setPostTitle}
+          postBody={postBody}
+          setPostBody={setPostBody}
+          handleSubmit={handleSubmit}
+        />} />
       
         <Route path="/post/:id" element={<PostPage posts={posts} handleDelete={handleDelete} />} />
       
--- src/NewPost.js
+++ src/NewPost.js
@@ -1,7 +1,36 @@
-const NewPost = () => {
+const NewPost = ({postTitle, setPostTitle, postBody, setPostBody, handleSubmit}) => {
   return (
-    <main>
-      <h1>NewPost</h1>
+    <main className="NewPost">
+      <h2 className="ml-3 text-2xl">New Post</h2>
+
+      <form className="newPostForm" onSubmit={handleSubmit}>
+        <div className="m-3">
+          <label htmlFor="title">Title:</label>
+          <div className="mt-3 border-2 w-50">
+            <input 
+              id="postTitle"
+              type="text"
+              required
+              value={postTitle}
+              onChange={(e) => setPostTitle(e.target.value)}
+            />
+          </div>
+        </div>
+        <div className="m-3">
+          <label htmlFor="body"> Body: </label>
+          <div className="mt-3 border-2 w-50">
+            <textarea
+              id="postBody"
+              required
+              value={postBody}
+              onChange={(e) => setPostBody(e.target.value)}
+            />
+          </div>
+        </div>
+        <div className="flex justify-center">
+          <button type="submit" className="w-40 m-3 p-2 text-center  bg-gray-500 text-white round border">Submit</button>
+        </div>
+      </form>
     </main>
   )
 }
